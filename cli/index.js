cd270a0d2f21038b2be81b523c39f255828cfd7b
andy <andyboythekid@gmail.com>
2020-12-11 12:11:38 -0800

    add custom config support

--- cli/index.js
+++ cli/index.js
@@ -9,17 +9,20 @@ const path = require("path");
 const build = require("./build");
 
 yargs(hideBin(process.argv)).command("build", "build the styles", (argv) => {
-  const styles = build();
-  
-  fs.writeFile(
-    "./styles.json",
-    JSON.stringify(styles, null, "\t"),
-    (err) => {
-      if (err !== null) {
-        console.log("ERR: ", err);
-      }
+  const configFileInCwd = path.resolve(process.cwd(), "tailwind.config.js");
+  let customConfig = {};
 
-      console.log("Done!");
+  if (fs.existsSync(configFileInCwd)) {
+    customConfig = require(configFileInCwd);
+  }
+
+  const styles = build(customConfig);
+
+  fs.writeFile("./styles.json", JSON.stringify(styles, null, "\t"), (err) => {
+    if (err !== null) {
+      console.log("ERR: ", err);
     }
-  );
+
+    console.log("Done!");
+  });
 }).argv;
