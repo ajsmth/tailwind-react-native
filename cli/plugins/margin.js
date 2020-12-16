41b0ab317706cd155cfab43e71d83feada38df99
andy <andyboythekid@gmail.com>
2020-12-16 15:37:35 -0800

    negative margins

--- cli/plugins/margin.js
+++ cli/plugins/margin.js
@@ -79,9 +79,63 @@ function margin(theme) {
         marginEnd: value,
       },
     };
+
+    margins[`-m-${marginName}`] = {
+      default: {
+        margin: value,
+      },
+    };
+
+    margins[`-mt-${marginName}`] = {
+      default: {
+        marginTop: value,
+      },
+    };
+
+    margins[`-mb-${marginName}`] = {
+      default: {
+        marginBottom: value,
+      },
+    };
+
+    margins[`-ml-${marginName}`] = {
+      default: {
+        marginLeft: value,
+      },
+    };
+
+    margins[`-mr-${marginName}`] = {
+      default: {
+        marginRight: value,
+      },
+    };
+
+    margins[`-mx-${marginName}`] = {
+      default: {
+        marginHorizontal: value,
+      },
+    };
+
+    margins[`-my-${marginName}`] = {
+      default: {
+        marginVertical: value,
+      },
+    };
+
+    margins[`-ms-${marginName}`] = {
+      default: {
+        marginStart: value,
+      },
+    };
+
+    margins[`-me-${marginName}`] = {
+      default: {
+        marginEnd: value,
+      },
+    };
   }
 
-  return margins
+  return margins;
 }
 
 module.exports = margin;
