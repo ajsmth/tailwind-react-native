af32bbd5adec34f00582e6510ff297a145304e6a
andy <andyboythekid@gmail.com>
2020-12-16 12:50:53 -0800

    chore

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 import { create } from "./style";
 import styles from "./styles.json";
@@ -9,6 +9,7 @@ export default function App() {
   return (
     <View style={style("flex-1 p-24")}>
       <View style={[style("w-24 h-24 bg-platform-red")]} />
+      <Text style={style("text-2xl")}>Hi</Text>
     </View>
   );
 }
