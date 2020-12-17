7beb24e10d75314b1ccd9267eeb83c7dc481fa9d
andy <andyboythekid@gmail.com>
2020-12-16 17:13:18 -0800

    fix config / output paths

--- cli/index.js
+++ cli/index.js
@@ -8,22 +8,38 @@ const path = require("path");
 
 const build = require("./build");
 
-yargs(hideBin(process.argv)).command("build", "build the styles", (argv) => {
-  const configFileInCwd = path.resolve(process.cwd(), "tailwind.config.js");
-  let customConfig = {};
+yargs(hideBin(process.argv))
+  .command("build", "build the styles", ({ argv }) => {
+    let configPath = argv.config || process.cwd();
+    const configFileInCwd = path.resolve(configPath, "tailwind.config.js");
+    let customConfig = {};
 
-  if (fs.existsSync(configFileInCwd)) {
-    customConfig = require(configFileInCwd);
-  }
-
-  const styles = build(customConfig);
-  const filePath = path.resolve(process.cwd(), "styles.json");
-
-  fs.writeFile(filePath, JSON.stringify(styles, null, "\t"), (err) => {
-    if (err !== null) {
-      console.log("ERR: ", err);
+    if (fs.existsSync(configFileInCwd)) {
+      customConfig = require(configFileInCwd);
+      console.log(`using custom config at ${configPath} to generate styles`);
     }
 
-    console.log("Done!");
-  });
-}).argv;
+    let outputPath = argv.out || process.cwd();
+    const styles = build(customConfig);
+    const filePath = path.resolve(outputPath, "styles.json");
+
+    console.log(`writing styles.json to ${filePath}/styles.json`);
+
+    fs.writeFile(filePath, JSON.stringify(styles, null, "\t"), (err) => {
+      if (err !== null) {
+        console.log("ERR: ", err);
+      }
+
+      console.log("Done!");
+    });
+  })
+  .option("config", {
+    alis: "c",
+    type: "string",
+    description: "path to a custom tailwind.config.js file",
+  })
+  .option("out", {
+    alis: "o",
+    type: "string",
+    description: "path to write the generated styles.json file",
+  }).argv;
