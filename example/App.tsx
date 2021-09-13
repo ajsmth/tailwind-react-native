e165db315d8b716753c63be0a281483d09911aa2
andy <40680668+ajsmth@users.noreply.github.com>
2021-09-13 10:20:59 -0700

    fix types

--- example/App.tsx
+++ example/App.tsx
@@ -1,34 +1,34 @@
 import * as React from "react";
-import { View } from "react-native";
+import {
+  View,
+  Image,
+  Text as RNText,
+} from "react-native";
+
 import sx from "./styles";
+
 import { create } from "react-native-primitives";
 
 // TODOs
 
 // -> fix theme so that it works with fast refresh
 // -> animation view / text
-// -> accept a function (e.g component) as the first arg
-// -> typescript infer the correct type based on the primitive option provided
-const Image = create("Image", {
-  base: {
-    
-  }
-})
+// -> apply arbitrary props for a given variant (e.g a11y traits for a heading)
 
-const Text = create("Text", {
-  props: {
+const Text = create(RNText, {
+  variants: {
     color: {
       success: sx("text-green-500"),
       danger: sx("text-red-500"),
     },
-  },
 
-  variants: {
-    h1: sx("text-4xl font-bold"),
-    h2: sx("text-3xl font-semibold"),
-    h3: sx("text-2xl font-medium"),
-    h4: sx("text-xl font-medium"),
-    p: sx("text-normal font-normal"),
+    variant: {
+      h1: sx("text-4xl font-bold"),
+      h2: sx("text-3xl font-semibold"),
+      h3: sx("text-2xl font-medium"),
+      h4: sx("text-xl font-medium"),
+      p: sx("text-normal font-normal"),
+    },
   },
 
   queries: {
@@ -39,14 +39,84 @@ const Text = create("Text", {
   },
 });
 
+const V = create(View, {
+  base: {},
+  variants: {
+    variant: {
+      base: {
+        flex: 1,
+      },
+    },
+
+    color: {
+      test: {},
+      success: sx("text-green-500"),
+      danger: sx("text-red-500"),
+    },
+  },
+
+  queries: {
+    appearance: {
+      light: {
+        flex: 1,
+      },
+    },
+  },
+});
+
 export default function App() {
   return (
-    <View style={[sx(["flex-1", "p-24"])]}>
-      <View style={sx("flex-1")}>
-        <Text variant="h1">
-          Hello
-        </Text>
-      </View>
+    <View style={sx("flex-1 pt-24")}>
+      <V variant="base" color="success" />
+      <Text variant="h2" color="success">
+        Hi
+      </Text>
     </View>
   );
 }
+
+create(Image, {
+  base: {},
+  variants: {},
+});
+
+// const View = createViewComponent({
+//   base: {},
+
+//   variants: {
+//     flex: {},
+//   },
+// });
+
+// const Image = createImageComponent({
+//   base: {
+//     resizeMode: "contain",
+//   },
+//   variants: {},
+// });
+
+// const Text = createTextComponent({
+//   base: sx("flex-1"),
+
+//   variants: {
+//     color: {
+//       success: sx("text-green-500"),
+//       danger: sx("text-red-500"),
+//     },
+
+//     variant: {
+//       h1: sx("text-4xl font-bold"),
+//       h2: sx("text-3xl font-semibold"),
+//       h3: sx("text-2xl font-medium"),
+//       h4: sx("text-xl font-medium"),
+//       p: sx("text-normal font-normal"),
+//     },
+//   },
+
+//   queries: {
+//     appearance: {
+//       // light: sx("border-2 border-red-200"),
+//       // dark: sx("border-4 border-blue-200"),
+//     },
+//   },
+// });
--- packages/react-native-primitives/src/index.ts
+++ packages/react-native-primitives/src/index.ts
@@ -1,13 +1,5 @@
 import * as React from "react";
 import {
-  Pressable,
-  PressableProps,
-  View,
-  ViewProps,
-  Image,
-  ImageProps,
-  Text,
-  TextProps,
   Appearance,
   AccessibilityInfo,
   AccessibilityChangeEvent,
@@ -20,78 +12,73 @@ import {
   TextStyle,
 } from "react-native";
 
-type PrimitiveOption = "Button" | "View" | "Image" | "Text";
-
-const PrimitiveMap: Record<PrimitiveOption, any> = {
-  Button: Pressable,
-  View: View,
-  Image: Image,
-  Text: Text,
-};
-
-type PropsMap = {
-  Button: PressableProps;
-  View: ViewProps;
-  Image: ImageProps;
-  Text: TextProps;
-};
-
-type StylesMap = {
-  Button: ViewStyle;
-  View: ViewStyle;
-  Image: ImageStyle;
-  Text: TextStyle;
-};
-
-type Options<Type> = {
-  [Property in keyof Type]?: keyof Type[Property];
-};
-
-type Queries = {
+type Queries<T> = {
   appearance?: {
-    light?: any;
-    dark?: any;
+    light?: T;
+    dark?: T;
   };
 
   accessibility?: {
-    boldText?: any;
-    grayScale?: any;
-    invertColors?: any;
-    reduceTransparency?: any;
+    boldText?: T;
+    grayScale?: T;
+    invertColors?: T;
+    reduceTransparency?: T;
   };
 
-  screen?: ScreenSizeQuery;
+  screen?: ScreenSizeQuery<T>;
 };
 
-type ScreenSizeQuery = {
-  height?: { [key: string]: any };
-  width?: { [key: string]: any };
+type ScreenSizeQuery<T> = {
+  height?: { [key: string]: T };
+  width?: { [key: string]: T };
 };
 
-type Variants<O extends PrimitiveOption> = {
-  base?: StyleProp<StylesMap[O]>;
-  props?: { [key: string]: { [key: string]: any } };
-  variants?: { [key: string]: any };
-  queries?: Queries;
+type Variants<Style> = {
+  base?: StyleProp<Style>;
+  variants?: { [key: string]: { [key: string]: StyleProp<Style> } };
+  queries?: Queries<Style>;
 };
 
-export function create<O extends PrimitiveOption, V extends Variants<O>>(
-  component: O,
-  variants: V
+type VariantProp<Type> = {
+  variant?: keyof Type;
+};
+
+type StyleFor<T> = T extends { style?: StyleProp<ImageStyle> }
+  ? StyleProp<ImageStyle>
+  : T extends { style?: StyleProp<TextStyle> }
+  ? StyleProp<TextStyle>
+  : T extends { style?: StyleProp<ViewStyle> }
+  ? StyleProp<ViewStyle>
+  : never;
+
+type Options<T> = {
+  base?: StyleFor<T>;
+  variants?: VariantMap<StyleFor<T>>;
+  queries?: Queries<StyleFor<T>>;
+};
+
+type VariantMap<T> = { [key: string]: { [key2: string]: T } };
+
+type Nested<Type> = {
+  [Property in keyof Type]?: keyof Type[Property];
+};
+
+export function create<T, O extends Options<T>>(
+  component: React.ComponentType<T>,
+  config: O
 ) {
-  const styleFn = getStylesFn(variants);
-  const Primitive = PrimitiveMap[component];
+  const styleFn = getStylesFn(config);
 
   function Component(
-    props: PropsMap[typeof component] &
-      Options<Variants<O>["props"]> & { variant?: keyof V["variants"] }
+    props: React.PropsWithChildren<T & Nested<typeof config["variants"]>>
   ) {
-    const qs = useQueryStyles(variants.queries);
-    const ss = useScreenSizeStyles(variants?.queries?.screen);
+    const qs = useQueryStyles(config.queries);
+    const ss = useScreenSizeStyles(config?.queries?.screen);
     const style = styleFn(props);
 
-    return React.createElement(Primitive, {
+    return React.createElement<T>(component, {
       ...props,
+      // @ts-ignore
       style: [style, props.style, qs, ss],
     });
   }
@@ -99,26 +86,16 @@ export function create<O extends PrimitiveOption, V extends Variants<O>>(
   return Component;
 }
 
-export function getStylesFn<T>(options: Variants<T>) {
-  let styles = options.base || {};
-  options.props = options.props || {};
+export function getStylesFn<T>(options: Options<T>) {
+  let styles: any = options.base || {};
 
   function handleVariantProps(props: any) {
-    options.props = options.props || {};
+    options.variants = options.variants || {};
 
     for (let key in props) {
-      if (options.props[key]) {
+      if (options.variants[key]) {
         const value = props[key];
-        const styleValue = options.props[key][value];
-
-        styles = StyleSheet.compose(styles, styleValue);
-      }
-
-      if (key === "variant") {
-        options.variants = options.variants || {};
-
-        const value = props[key];
-        const styleValue = options.variants[value] || {};
+        const styleValue = options.variants[key][value];
 
         styles = StyleSheet.compose(styles, styleValue);
       }
@@ -132,7 +109,7 @@ export function getStylesFn<T>(options: Variants<T>) {
 
 const noop = () => {};
 
-function useQueryStyles(queries?: Queries) {
+function useQueryStyles<T>(queries?: Queries<T>) {
   const [qs, setState] = React.useState({});
 
   const update = React.useCallback((nextState) => {
@@ -201,8 +178,8 @@ function useQueryStyles(queries?: Queries) {
   return qs;
 }
 
-function registerThemeListener(
-  queries: Queries["appearance"],
+function registerThemeListener<T>(
+  queries: Queries<T>["appearance"],
   update: any
 ): () => void {
   if (!queries?.dark && !queries?.light) {
@@ -232,11 +209,11 @@ function registerThemeListener(
   return () => Appearance.removeChangeListener(handleAppearanceChange);
 }
 
-function registerA11yListener(
-  queries: Queries["accessibility"],
+function registerA11yListener<T>(
+  queries: Queries<T>["accessibility"],
   update: any,
   config: {
-    queryKey: keyof Queries["accessibility"];
+    queryKey: keyof Queries<T>["accessibility"];
     a11yEventName: AccessibilityChangeEventName;
     getValueAsync: () => Promise<boolean>;
   }
@@ -266,7 +243,7 @@ function registerA11yListener(
     AccessibilityInfo.removeEventListener(config.a11yEventName, handleChange);
 }
 
-function useScreenSizeStyles(sizes?: ScreenSizeQuery) {
+function useScreenSizeStyles<T>(sizes?: ScreenSizeQuery<T>) {
   const [widthStyles, setWidthStyles] = React.useState({});
   const [heightStyles, setHeightStyles] = React.useState({});
 
@@ -274,17 +251,14 @@ function useScreenSizeStyles(sizes?: ScreenSizeQuery) {
 
   React.useEffect(() => {
     if (sizes && sizes.width) {
-      let updates = {};
+      let updates: any = {};
 
       for (let key in sizes.width) {
         const expression = `${width} ${key}`;
         try {
           if (eval(expression) == true) {
             const queryStyles = sizes.width[key];
-            updates = {
-              ...updates,
-              ...queryStyles,
-            };
+            updates = StyleSheet.compose(updates, queryStyles);
           }
         } catch (error) {
           console.warn(
@@ -299,17 +273,14 @@ function useScreenSizeStyles(sizes?: ScreenSizeQuery) {
 
   React.useEffect(() => {
     if (sizes && sizes.height) {
-      let updates = {};
+      let updates: any = {};
 
       for (let key in sizes.height) {
         const expression = `${height} ${key}`;
         try {
           if (eval(expression) == true) {
             const queryStyles = sizes.height[key];
-            updates = {
-              ...updates,
-              ...queryStyles,
-            };
+            updates = StyleSheet.compose(updates, queryStyles);
           }
         } catch (error) {
           console.warn(
